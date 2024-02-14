813132fdeecf0f5638932cd491dffe85ab8701d1
Victor Hugo <victor.hiraiftw@outlook.com>
2024-02-14 10:17:22 -0300

    add pacotes rotas e controllers

---
+++ api/src/controllers/usuarios.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+// CriarUsuario insere um usuário no banco de dados
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Criando Usuário!"))
+}
+
+// BuscarUsuarios busca todos os usuários salvos no banco
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando todos os usuários!"))
+}
+
+// BuscarUsuario busca um usuário salvo no banco
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando um  usuário!"))
+}
+
+// AtualizarUsuario altera as informações de  um usuário no banco
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Atualizando Usuário!"))
+}
+
+// DeletarUsuario exclui as informações de um usuário no banco
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Deletando Usuário!"))
+}
